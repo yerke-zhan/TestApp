5003454c11b470c482db7c7afff95984315b654d
Yerke <yerkezhanm@gmail.com>
2025-03-14 17:13:12 +0500

    Hide the login button

--- 27Week-HW-1/src/App.jsx
+++ 27Week-HW-1/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <>
-      
-      <RandomColorBox/>
+{/*       
+      <RandomColorBox/> */}
       <LoginForm/>
     </>
   )
--- 27Week-HW-1/src/components/LoginForm.jsx
+++ 27Week-HW-1/src/components/LoginForm.jsx
@@ -1,35 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function LoginForm() {
-const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [showRegister, setShowRegister] = useState(false);
 
   return (
     <div className='login'>
       {!isLoggedIn ? (
-        <div >
-          <h2 >Жүйеге кіру қажет</h2>
-          <button onClick={() => setIsLoggedIn(true)}
-          >Login </button>
-            
-         
-          <br /><br />
+        <div>
+          <h2>Жүйеге кіру қажет</h2>
+
+          {/* Hide login button when showRegister is true */}
           {!showRegister && (
-            <button onClick={() => setShowRegister(true)}>Register</button>
+            <button onClick={() => setIsLoggedIn(true)}>Login</button>
           )}
-          {showRegister && (<div ><input type="text"placeholder="Email/Телефон"/>
-            <input type="password"
-                placeholder="Құпия сөз"/><button onClick={() => setShowRegister(false)}>Register</button>
+
+          <br /><br />
+
+          {!showRegister ? (
+            <button onClick={() => setShowRegister(true)}>Register</button>
+          ) : (
+            <div>
+              <input type="text" placeholder="Email/Телефон" />
+              <input type="password" placeholder="Құпия сөз" />
+              <button onClick={() => setShowRegister(false)}>Register</button>
             </div>
           )}
-        </div>) : ( <div >
-          <h2 >Бастапқы бетке қош келдіңіз</h2>
-          <button onClick={() => setIsLoggedIn(false)}>Log out</button></div>
+        </div>
+      ) : (
+        <div>
+          <h2>Бастапқы бетке қош келдіңіз</h2>
+          <button onClick={() => setIsLoggedIn(false)}>Log out</button>
+        </div>
       )}
     </div>
   );
 }
-
-
-   
-  
